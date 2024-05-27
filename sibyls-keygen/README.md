52930758db5f201f90e4e038bb6554a52417e551
Max Gravitt <max.gravitt@gmail.com>
2024-05-27 12:23:12 -0400

    added tests, removed output flag

--- sibyls-keygen/README.md
+++ sibyls-keygen/README.md
@@ -31,8 +31,9 @@ Public Key: 02f3c64779a7c570bb6fbe297226de0286649b3266fb18aeaf3906aeb3de11c8ff
 
 2. **Generate a key pair and save to a file:**
 ```sh
-./target/release/sibyls-keygen generate --output file --output-file /path/to/your/keystore.txt
+./target/release/sibyls-keygen generate --output-file /path/to/your/keystore.txt
 ```
+If the file already exists, the command will fail unless the `--overwrite` flag is provided.
 
 ### Inspect Command
 The `inspect` command accepts a private key from a file, environment variable, or command line argument and prints the corresponding public key.
--- sibyls-keygen/src/main.rs
+++ sibyls-keygen/src/main.rs
@@ -1,13 +1,6 @@
-use clap::{Parser, Subcommand, ValueEnum};
-use hex;
+use clap::{Parser, Subcommand};
 use secp256k1::{rand, Secp256k1, SecretKey, PublicKey};
-use std::{fs::File, io::{Read, Write}, path::PathBuf, env};
-
-fn get_default_keystore_path() -> PathBuf {
-    let mut path = env::current_dir().unwrap();
-    path.push("keystore");
-    path
-}
+use std::{fs::File, io::{self, Read, Write}, path::{Path, PathBuf}, env};
 
 #[derive(Parser)]
 #[clap(author, version, about)]
@@ -20,13 +13,13 @@ struct Args {
 enum Commands {
     /// Generates a new keypair
     Generate {
-        /// Output the key to the console or save to a file
-        #[clap(short='o', long, value_enum, default_value = "console")]
-        output: OutputType,
-        /// Path and filename to save the key, required if output is "file"
-        #[clap(short='f', long, value_name = "FILE", value_hint = clap::ValueHint::FilePath, requires_if("output", "file"))]
-        #[arg(default_value= get_default_keystore_path().into_os_string())]
+        /// Path and filename to save the key, if not provided, key will be printed to console
+        #[clap(short='f', long, value_name = "FILE", value_hint = clap::ValueHint::FilePath)]
         output_file: Option<PathBuf>,
+
+        /// Overwrite the file if it already exists
+        #[clap(long)]
+        overwrite: bool,
     },
     /// Inspects a private key and prints the corresponding public key
     Inspect {
@@ -34,12 +27,6 @@ enum Commands {
         #[clap(short, long, env, value_name = "KEY")]
         key: String,
     },
-}
-
-#[derive(Debug, ValueEnum, Clone)]
-enum OutputType {
-    Console,
-    File,
 }
 
 fn main() {
@@ -50,47 +37,148 @@ fn main() {
     let secp = Secp256k1::new();
 
     match args.command {
-        Commands::Generate { output, output_file } => {
-            let secret_key = SecretKey::new(&mut rand::thread_rng());
-            let public_key = PublicKey::from_secret_key(&secp, &secret_key);
-
-            let secret_key_hex = hex::encode(secret_key.as_ref());
-            let public_key_hex = hex::encode(public_key.serialize());
-
-            match output {
-                OutputType::Console => {
-                    println!("Secret Key: {}", secret_key_hex);
-                    println!("Public Key: {}", public_key_hex);
-                },
-                OutputType::File => {
-                    if let Some(path) = output_file {
-                        let mut file = File::create(path.clone()).expect("Unable to create file");
-                        writeln!(file, "{}", secret_key_hex).expect("Unable to write to file");
-                        println!("Private key has been saved to the file: {}", path.display());
-                    } else {
-                        eprintln!("Output file path is required when output type is 'file'.");
+        Commands::Generate { output_file, overwrite } => {
+            match generate_keypair(&secp, output_file.as_deref(), overwrite) {
+                Ok((secret_key_hex, public_key_hex)) => {
+                    if output_file.is_none() {
+                        println!("Secret Key: {}", secret_key_hex);
+                        println!("Public Key: {}", public_key_hex);
                     }
                 },
+                Err(e) => eprintln!("Error: {}", e),
             }
         },
         Commands::Inspect { key } => {
-            let secret_key_hex = if PathBuf::from(&key).exists() {
-                let mut file = File::open(key).expect("Unable to open file");
-                let mut contents = String::new();
-                file.read_to_string(&mut contents).expect("Unable to read file");
-                contents.trim().to_string()
-            } else if let Ok(env_key) = env::var(&key) {
-                env_key
-            } else {
-                key
-            };
-
-            let secret_key_bytes = hex::decode(secret_key_hex).expect("Invalid hex string");
-            let secret_key = SecretKey::from_slice(&secret_key_bytes).expect("Invalid secret key");
-            let public_key = PublicKey::from_secret_key(&secp, &secret_key);
-            let public_key_hex = hex::encode(public_key.serialize());
-
-            println!("Public Key: {}", public_key_hex);
+            match inspect_key(&secp, &key) {
+                Ok(public_key_hex) => println!("Public Key: {}", public_key_hex),
+                Err(e) => eprintln!("Error: {}", e),
+            }
         },
+    }
+}
+
+fn generate_keypair(secp: &Secp256k1<secp256k1::All>, output_file: Option<&Path>, overwrite: bool) -> Result<(String, String), io::Error> {
+    let secret_key = SecretKey::new(&mut rand::thread_rng());
+    let secret_key_hex = hex::encode(secret_key.as_ref());
+
+    let public_key = PublicKey::from_secret_key(secp, &secret_key);
+    let public_key_hex = hex::encode(public_key.serialize());
+
+    if let Some(path) = output_file {
+        if path.exists() && !overwrite {
+            return Err(io::Error::new(io::ErrorKind::AlreadyExists, format!("File {} already exists", path.display())));
+        }
+        let mut file = File::create(path)?;
+        writeln!(file, "{}", secret_key_hex)?;
+        println!("Private key has been saved to the file: {}", path.display());
+    }
+
+    Ok((secret_key_hex, public_key_hex))
+}
+
+fn inspect_key(secp: &Secp256k1<secp256k1::All>, key: &str) -> Result<String, io::Error> {
+    let secret_key_hex = if Path::new(key).exists() {
+        let mut file = File::open(key)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        contents.trim().to_string()
+    } else if let Ok(env_key) = env::var(key) {
+        env_key
+    } else {
+        key.to_string()
+    };
+
+    let secret_key_bytes = hex::decode(&secret_key_hex).map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid hex string"))?;
+    let secret_key = SecretKey::from_slice(&secret_key_bytes).map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid secret key"))?;
+    let public_key = PublicKey::from_secret_key(secp, &secret_key);
+    let public_key_hex = hex::encode(public_key.serialize());
+
+    Ok(public_key_hex)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn test_generate_keypair_console() {
+        let secp = Secp256k1::new();
+        let (secret_key_hex, public_key_hex) = generate_keypair(&secp, None, false).unwrap();
+        
+        assert_eq!(secret_key_hex.len(), 64);
+        assert_eq!(public_key_hex.len(), 66);
+    }
+
+    #[test]
+    fn test_inspect_key_from_str() {
+        let secp = Secp256k1::new();
+        let secret_key_hex = "c1b8c027c89bb2c8b8db0b93721e1e9885e92b6b68d44c1f9026f83e5a2763df";
+        let expected_public_key_hex = "0244990384b935f24a9c0babd4642113678c3dde5dc7904589497bc435f90bfe1f";
+        let public_key_hex = inspect_key(&secp, secret_key_hex).unwrap();
+                
+        assert_eq!(public_key_hex.len(), 66);
+        assert_eq!(public_key_hex, expected_public_key_hex);
+    }
+
+    #[test]
+    fn test_generate_keypair_file() {
+        use secp256k1::rand::Rng;
+        let secp = Secp256k1::new();
+        let random_filename: String = rand::thread_rng()
+            .sample_iter(&rand::distributions::Alphanumeric)
+            .take(10)
+            .map(char::from)
+            .collect();
+        let path = env::current_dir().unwrap().join(random_filename);
+
+        let (secret_key_hex, public_key_hex) = generate_keypair(&secp, Some(&path), false).unwrap();
+
+        let mut read_contents = String::new();
+        File::open(&path).expect("Unable to open file")
+            .read_to_string(&mut read_contents).expect("Unable to read file");
+
+        assert_eq!(read_contents.trim(), secret_key_hex);
+        assert_eq!(secret_key_hex.len(), 64);
+        assert_eq!(public_key_hex.len(), 66);
+
+        fs::remove_file(&path).expect("Unable to delete test file");
+    }
+
+    #[test]
+    fn test_generate_keypair_file_overwrite() {
+        use secp256k1::rand::Rng;
+        let secp = Secp256k1::new();
+        let random_filename: String = rand::thread_rng()
+            .sample_iter(&rand::distributions::Alphanumeric)
+            .take(10)
+            .map(char::from)
+            .collect();
+        let path = env::current_dir().unwrap().join(random_filename);
+
+        // First, create the file without overwriting
+        let (secret_key_hex1, public_key_hex1) = generate_keypair(&secp, Some(&path), false).unwrap();
+
+        // Ensure file content is as expected
+        let mut read_contents = String::new();
+        File::open(&path).expect("Unable to open file")
+            .read_to_string(&mut read_contents).expect("Unable to read file");
+
+        assert_eq!(read_contents.trim(), secret_key_hex1);
+
+        // Now, overwrite the file
+        let (secret_key_hex2, public_key_hex2) = generate_keypair(&secp, Some(&path), true).unwrap();
+
+        // Ensure file content is updated
+        let mut read_contents = String::new();
+        File::open(&path).expect("Unable to open file")
+            .read_to_string(&mut read_contents).expect("Unable to read file");
+
+        assert_eq!(read_contents.trim(), secret_key_hex2);
+
+        assert_ne!(secret_key_hex1, secret_key_hex2);
+        assert_ne!(public_key_hex1, public_key_hex2);
+
+        fs::remove_file(&path).expect("Unable to delete test file");
     }
 }
