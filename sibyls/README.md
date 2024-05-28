99b5f2e85a32867ffad84575340bca6f8edd6e0f
Max Gravitt <max.gravitt@gmail.com>
2024-05-28 08:00:30 -0400

    transitioning sibyls to a crate in the workspace

---
+++ sibyls/README.md
@@ -0,0 +1,4 @@
+# sibyls
+a numeric (and extensible) oracle implementation for bitcoin. 
+
+See the repo level README.md for more information. 
--- sibyls/src/oracle/pricefeeds/kraken.rs
+++ sibyls/src/oracle/pricefeeds/kraken.rs
@@ -85,9 +85,13 @@ mod tests {
     async fn retrieve() {
         let feed = Kraken {};
         let price = feed.retrieve_price(BTCUSD, OffsetDateTime::now_utc()).await;
-        match price {
-            Ok(_) => assert!(true),
-            Err(_) => assert!(false, "{:#?}", &price),
-        }
+
+        assert!(price.is_ok(), "Failed to retrieve price: {:#?}", price);
+        println!("Retrieved price: {:?}", price.unwrap());
+
+        // match price {
+        //     Ok(_) => assert!(true),
+        //     Err(_) => assert!(false, "{:#?}", &price),
+        // }
     }
 }
