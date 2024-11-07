fca55f62df8e8e841b74f6925e2ff84638cd44b1
kevkevinpal <oapallikunnel@gmail.com>
2024-11-07 16:54:43 -0500

    test: added coverage to inspect_key for key path
    Added addtional coverage for inspect_key when a path is given instead of
    a hex value

--- sibyls-keygen/src/main.rs
+++ sibyls-keygen/src/main.rs
@@ -145,6 +145,27 @@ mod tests {
     }
 
     #[test]
+    fn test_inspect_key_from_path() {
+        use secp256k1::rand::Rng;
+        let secp = Secp256k1::new();
+        let random_filename: String = rand::thread_rng()
+            .sample_iter(&rand::distributions::Alphanumeric)
+            .take(10)
+            .map(char::from)
+            .collect();
+        let path = env::current_dir().unwrap().join(random_filename);
+
+        let (_, expected_public_key_hex) = generate_keypair(&secp, Some(&path), false).unwrap();
+        
+        let path_string = path.to_str().unwrap();
+        let public_key_hex = inspect_key(&secp, path_string).unwrap();
+        assert_eq!(public_key_hex.len(), 66);
+        assert_eq!(public_key_hex, expected_public_key_hex);
+
+        fs::remove_file(&path).expect("Unable to delete test file");
+    }
+
+    #[test]
     fn test_generate_keypair_file() {
         use secp256k1::rand::Rng;
         let secp = Secp256k1::new();
